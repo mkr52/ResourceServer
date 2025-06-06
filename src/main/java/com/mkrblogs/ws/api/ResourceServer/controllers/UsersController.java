e071b0bacbf74f04d860c589cf5821b5f7420e3f
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-06-06 17:28:24 +0530

    method level security: postauthorize

--- src/main/java/com/mkrblogs/ws/api/ResourceServer/controllers/UsersController.java
+++ src/main/java/com/mkrblogs/ws/api/ResourceServer/controllers/UsersController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mkrblogs.ws.api.ResourceServer.response.UserRest;
+
 @RestController
 @RequestMapping("/users")
 public class UsersController {
@@ -29,4 +31,9 @@ public class UsersController {
         return "Deleted user with id " + id + " and JWT subject " + jwt.getSubject();
     }
 
+	@PostAuthorize("returnObject.userId == #jwt.subject")
+	@GetMapping(path = "/{id}")
+    public UserRest getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return new UserRest("Mohit", "Kumar","360c82d9-a4a5-4b16-a319-b085502d0eea");
+    }
 }
