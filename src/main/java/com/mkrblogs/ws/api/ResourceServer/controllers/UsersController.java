4ae08aee81ac6ece14fb6f76dedbd53d4e37a459
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-06-06 17:18:52 +0530

    method level security: preauthorize

--- src/main/java/com/mkrblogs/ws/api/ResourceServer/controllers/UsersController.java
+++ src/main/java/com/mkrblogs/ws/api/ResourceServer/controllers/UsersController.java
@@ -1,6 +1,13 @@
 package com.mkrblogs.ws.api.ResourceServer.controllers;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +19,14 @@ public class UsersController {
 	public String status() {
 		return "Working...";
 	}
+	
+	@PreAuthorize("hasAuthority('ROLE_developer') and #id == #jwt.subject")
+//	@PreAuthorize(value = "hasRole('developer')")
+//	@PostAuthorize(value = "")
+//	@Secured("ROLE_developer")
+	@DeleteMapping(path = "/{id}")
+    public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        return "Deleted user with id " + id + " and JWT subject " + jwt.getSubject();
+    }
 
 }
--- src/main/java/com/mkrblogs/ws/api/ResourceServer/security/WebSecurity.java
+++ src/main/java/com/mkrblogs/ws/api/ResourceServer/security/WebSecurity.java
@@ -3,12 +3,14 @@ package com.mkrblogs.ws.api.ResourceServer.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity(securedEnabled=true, prePostEnabled=true)
 @EnableWebSecurity
 public class WebSecurity {
 
