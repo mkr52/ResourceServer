1691f12232ae9d577d07d8adb3d37b7f279ce4f0
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-06-06 13:28:56 +0530

    Role based access control with keycloak

--- src/main/java/com/mkrblogs/ws/api/ResourceServer/security/WebSecurity.java
+++ src/main/java/com/mkrblogs/ws/api/ResourceServer/security/WebSecurity.java
@@ -5,21 +5,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
-	
-	
+
 	@Bean
 	SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		
-		http.authorizeHttpRequests(authz -> 
-		authz
-		.requestMatchers(HttpMethod.GET, "/users").hasAnyAuthority("SCOPE_profile")
-		.anyRequest().authenticated())
-		.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
+		JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+		jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleconverter());
+
+		http.authorizeHttpRequests(authz -> authz.requestMatchers(HttpMethod.GET, "/users/status/check")
+//		.hasAnyAuthority("SCOPE_profile")
+				.hasRole("developer")
+//				.hasAnyAuthority("ROLE_developer")
+				// .hasAnyRole("developer", "user")
+				.anyRequest().authenticated())
+					.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)));
 		return http.build();
 	}
 
