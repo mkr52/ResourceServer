f2c4538717cfd4b8bd37fab09122ad91f580c22e
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-06-10 16:26:22 +0530

    eureka and resource server instance id property\n to show multiple running instances on eureka

--- src/main/java/com/mkrblogs/ws/api/ResourceServer/ResourceServerApplication.java
+++ src/main/java/com/mkrblogs/ws/api/ResourceServer/ResourceServerApplication.java
@@ -2,8 +2,10 @@ package com.mkrblogs.ws.api.ResourceServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ResourceServerApplication {
 
 	public static void main(String[] args) {
